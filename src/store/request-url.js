6a9bb3ca767e703190dd2dbe9393a1a0c20b252a
Aleksandr M <site.rv6@gmail.com>
2020-10-25 20:30:52 +0300

    amk

--- src/store/request-url.js
+++ src/store/request-url.js
@@ -10,5 +10,5 @@ export const user = {
 };
 export const reports = {
   chartData: `${apiUrl}api/logs`,
-  downloadLink: `${apiUrl}api/logs-download?token={userToken}&range={range}&type=csv&query[]=temperature&query[]=humidity&query[]=pressure&query[]=rain`,
+  downloadLink: `${apiUrl}logs-download?token={userToken}&range={range}&type=csv&query[]=temperature&query[]=humidity&query[]=pressure&query[]=rain`,
 };
